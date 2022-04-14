3b280fb712c41ea1fdf3ccfb9e7f0cb5ff997bdb
dueruen <32197042+dueruen@users.noreply.github.com>
2022-04-14 14:31:37 +0200

    working kafka setup

--- application/confluent-demo/consumer.go
+++ application/confluent-demo/consumer.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func startConsumer(bootstrapServer string, groupId string, topic *string, consumerShutdown chan bool) {
-	fmt.Println("Starting consumer - topic: " + *topic)
+	fmt.Printf("Starting consumer - server: %s - groupId: %s - topic: %s\n", bootstrapServer, groupId, *topic)
 
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
@@ -25,76 +27,46 @@ func startConsumer(bootstrapServer string, groupId string, topic *string, consum
 		os.Exit(1)
 	}
 
-	run := true
+	err = c.SubscribeTopics([]string{*topic}, nil)
+	if err != nil {
+		fmt.Printf("Failed to subsribe to topic: %s  - Error: %s", *topic, err)
+		os.Exit(1)
+	}
 
-	for run {
+	totalCount := 0
+	run := true
+	fmt.Println("Consumer running")
+	for run == true {
 		select {
 		case sig := <-sigchan:
-			fmt.Printf("Caught signal %v: terminating\n", sig)
+			fmt.Printf("Caught signal %v: terminating consumer\n", sig)
+			consumerShutdown <- true
 			run = false
 		default:
-			ev := c.Poll(100)
-			if ev == nil {
+			msg, err := c.ReadMessage(100 * time.Millisecond)
+			if err != nil {
+				// Errors are informational and automatically handled by the consumer
 				continue
 			}
 
-			switch e := ev.(type) {
-			case *kafka.Message:
-				fmt.Printf("%% Message on %s:\n%s\n",
-					e.TopicPartition, string(e.Value))
-				if e.Headers != nil {
-					fmt.Printf("%% Headers: %v\n", e.Headers)
-				}
-			case kafka.Error:
-				// Errors should generally be considered
-				// informational, the client will try to
-				// automatically recover.
-				// But in this example we choose to terminate
-				// the application if all brokers are down.
-				fmt.Fprintf(os.Stderr, "%% Error: %v: %v\n", e.Code(), e)
-				if e.Code() == kafka.ErrAllBrokersDown {
-					run = false
-				}
-			default:
-				fmt.Printf("Ignored %v\n", e)
+			if msg.Headers != nil {
+				fmt.Printf("%% Headers: %v\n", msg.Headers)
 			}
+
+			recordKey := string(msg.Key)
+			message := msg.Value
+			data := KafkaMessage{}
+			err = json.Unmarshal(message, &data)
+			if err != nil {
+				fmt.Printf("Failed to decode JSON at offset %d: %v", msg.TopicPartition.Offset, err)
+				continue
+			}
+			count := data.Count
+			totalCount = count
+			fmt.Printf("Consumed record with key %s and value %d, and updated total count to %d -- Message was: %s\n", recordKey, data.Count, totalCount, data.Message)
 		}
 	}
 
-	// err = c.SubscribeTopics([]string{*topic}, nil)
-	// if err != nil {
-	// 	fmt.Printf("Failed to subsribe to topic: %s  - Error: %s", *topic, err)
-	// 	os.Exit(1)
-	// }
-
-	// totalCount := 0
-	// run := true
-	// fmt.Println("Consumer running")
-	// for run == true {
-	// 	select {
-	// 	case sig := <-sigchan:
-	// 		fmt.Printf("Caught signal %v: terminating consumer\n", sig)
-	// 		consumerShutdown <- true
-	// 		run = false
-	// 	default:
-	// 		msg, err := c.ReadMessage(100 * time.Millisecond)
-	// 		if err != nil {
-	// 			// Errors are informational and automatically handled by the consumer
-	// 			continue
-	// 		}
-	// 		recordKey := string(msg.Key)
-	// 		message := msg.Value
-	// 		data := KafkaMessage{}
-	// 		err = json.Unmarshal(message, &data)
-	// 		if err != nil {
-	// 			fmt.Printf("Failed to decode JSON at offset %d: %v", msg.TopicPartition.Offset, err)
-	// 			continue
-	// 		}
-	// 		count := data.Count
-	// 		totalCount += count
-	// 		fmt.Printf("Consumed record with key %s and value %d, and updated total count to %d -- Message was: %s\n", recordKey, data.Count, totalCount, data.Message)
-	// 	}
-	// }
-
+	fmt.Println("END of consumer")
 	c.Close()
 }
